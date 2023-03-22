8f75422bf170b7453aa7385608773fae1586aeba
Alan <aalan.lima@live.com>
2023-03-22 15:17:45 -0300

    Create CircleWaves component

--- src/objects/MainScreen/index.js
+++ src/objects/MainScreen/index.js
@@ -1,4 +1,5 @@
 import ArchedImage from "../../components/ArchedImage"
+import CircleWaves from "../../components/CircleWaves/index,js"
 import MainButton from "../../components/MainButton"
 import MainTitle from "../../components/MainTitle"
 import "./style.css"
@@ -8,8 +9,9 @@ const MainScreen = ()=>{
         <div class="main-screen">
             <div class="main-screen__left">
                 ${MainTitle('the modern house')}
-                ${MainButton('REQUEST A CALL')}
+                ${MainButton('REQUEST A CALL','main-button--large')}
             </div>
+            ${CircleWaves()}
             ${ArchedImage()}
         </div>
         
