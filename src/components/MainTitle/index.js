ef961d4f884802b021e99aedb15adb2caa9d8ec2
Alan <aalan.lima@live.com>
2023-03-22 13:22:04 -0300

    Create MainTitle component

---
+++ src/components/MainTitle/index.js
@@ -0,0 +1,11 @@
+import "./style.css"
+
+const MainTitle = (content)=>{
+    return/* html */ `
+        <h1 class="main-title">
+            ${content}
+        </h1>
+    `
+}
+
+export default MainTitle
