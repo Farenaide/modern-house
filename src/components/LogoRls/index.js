e496514e6c7383e5c130e65a0a068684769dd76b
Alan <aalan.lima@live.com>
2023-03-21 23:21:42 -0300

    create LogoRls component

---
+++ src/components/LogoRls/index.js
@@ -0,0 +1,11 @@
+import "./style.css"
+
+const LogoRls = ()=>{
+    return /* html */`
+        <figure class="logo-rls">
+            <span>RLS</span>
+        </figure>
+    `
+}
+
+export default LogoRls
--- src/components/MainHeader/index.js
+++ src/components/MainHeader/index.js
@@ -1,4 +1,5 @@
 import HeaderButtons from "../../objects/HeaderButtons"
+import LogoRls from "../LogoRls"
 import MainButton from "../MainButton"
 import NavBar from "../NavBar"
 import "./style.css"
@@ -6,6 +7,7 @@ import "./style.css"
 const MainHeader = ()=>{
     return/* html */`
         <header class="main-header">
+            ${LogoRls()}
             ${NavBar()}
             ${HeaderButtons()}
         </header>
