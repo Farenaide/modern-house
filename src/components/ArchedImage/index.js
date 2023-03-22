be9936750750c968b33f422b9627381a3aa96d2a
Alan <aalan.lima@live.com>
2023-03-22 15:44:56 -0300

    Add visual element on ArchedImage

--- src/components/ArchedImage/index.js
+++ src/components/ArchedImage/index.js
@@ -3,6 +3,7 @@ import "./style.css"
 const ArchedImage = ()=>{
     return /* html */`
         <figure class="arched-image">
+            <img class="arched-image__welcome" src="src/assets/visual/welcome.png">
             <img class="arched-image__img" src="src/assets/pictures/Rectangle 207.png" alt="">
         </figure>
     `
