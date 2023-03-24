cdbca5d0cfc8147c701add657461f364a068d266
Alan <aalan.lima@live.com>
2023-03-24 19:00:00 -0300

    visual adjustments

--- src/objects/MainCarousel/index.js
+++ src/objects/MainCarousel/index.js
@@ -1,14 +1,19 @@
 import CarouselImage from "../../components/CarouselImage"
+import ChoiceMenu from "../../components/ChoiceMenu"
 import "./style.css"
 
 const MainCarousel = ()=>{
     return/* html */ `
         <div class="main-carousel">
-            ${CarouselImage('apartment2')}
             ${CarouselImage('apartment1')}
+            ${CarouselImage('apartment2')}
+            ${ChoiceMenu()}
             ${CarouselImage('apartment3')}
-        </div
+            ${CarouselImage('apartment4')}
+        </div>
     `
 }
+
+
 
 export default MainCarousel
