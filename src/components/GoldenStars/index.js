4245ea73a5297768a2c9fddd4a2d1f42e5e0161e
Alan <aalan.lima@live.com>
2023-03-28 23:54:56 -0300

    Change GoldenStars component to work with mask

--- src/components/GoldenStars/index.js
+++ src/components/GoldenStars/index.js
@@ -1,8 +1,8 @@
 import "./style.css"
 
-const GoldenStars = (value = '5')=>{
+const GoldenStars = (value = '3')=>{
     return/* html */ `
-        <div class="golden-stars">
+        <div class="golden-stars mask">
             <progress class="golden-stars__progress" max='5' value=${value}>${value} estrelas</progress>
         </div>
     `
