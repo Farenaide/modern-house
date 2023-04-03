5647f3a16f98abba7d7dc7644400ab5af80050bc
Alan <aalan.lima@live.com>
2023-04-03 18:40:40 -0300

    Visual adjustments

--- main.js
+++ main.js
@@ -7,11 +7,10 @@ import "./src/styles/generic/reset.css"
 
 import MainHeader from "./src/components/MainHeader";
 import MainScreen from "./src/objects/MainScreen";
-import CarouselWrapper from "./src/objects/CarouselWrapper";
 import DescriptionWrapper from "./src/objects/DescriptionWrapper";
 import DescriptionWrapper2 from "./src/objects/DescriptionWrapper2";
-import CarouselComment from "./src/components/CarouselComment";
-import GoldenStars from "./src/components/StarRating";
+import ApartmentsCarouselWrapper from "./src/objects/ApartmentsCarouselWrapper";
+import CommentsCarouselWrapper from "./src/objects/CommentsCarouselWrapper";
 
 const $root = document.querySelector('#root');
 
@@ -20,10 +19,10 @@ $root.insertAdjacentHTML('beforeend',
   `
   ${MainHeader()}
   ${MainScreen()}
-  ${CarouselWrapper()}
+  ${ApartmentsCarouselWrapper()}
   ${DescriptionWrapper()}
   ${DescriptionWrapper2()}
-  ${CarouselComment('user1.png','4')}
+  ${CommentsCarouselWrapper()}
   `
 )
 
--- src/components/CarouselImage/index.js
+++ src/components/CarouselImage/index.js
@@ -1,10 +1,10 @@
 import "./style.css"
 
-const CarouselImage = (content, id='0')=>{
+const CarouselImage = (content)=>{
     const contentUrl = new URL(`/src/assets/${content}`, import.meta.url).href
 
     return/* html */ `
-        <figure class="carousel-image" id=${id}>
+        <figure class="carousel-image">
             <img class="carousel-image__img" src=${contentUrl} alt="">
         </figure>
     `
