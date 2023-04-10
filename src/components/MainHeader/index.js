6750886610f7875808530dfc4233a84ae7414e4d
Alan <aalan.lima@live.com>
2023-04-10 15:34:16 -0300

    Create media queiries for MainHeader components

--- src/components/MainHeader/index.js
+++ src/components/MainHeader/index.js
@@ -5,8 +5,12 @@ import "./style.css"
 const MainHeader = ()=>{
     return/* html */`
         <header class="main-header">
-            ${LogoRls()}
-            ${NavBar()} 
+            <div class="main-header__logo">
+                ${LogoRls()}
+            </div>
+            <div class="main-header__nav">
+                ${NavBar()} 
+            </div>
         </header>
     `
 }
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -3,16 +3,32 @@ import "./style.css"
 
 const NavBar = ()=>{
     return /* html */`
-    <nav class="nav-bar">
-        <ul class="nav-bar__list">
-            <li><a class="nav-bar__link" href="#">HOME</a></li>
-            <li><a class="nav-bar__link" href="#">ABOUT US</a></li>
-            <li><a class="nav-bar__link" href="#">SERVICE</a></li>
-            <li><a class="nav-bar__link" href="#">CONTACT US</a></li>
-        </ul>
-        ${HeaderButtons()}
+    <nav class="nav-bar">     
+        <div class="nav-bar__wrapper">
+            <input type="checkbox" id="nav-bar__menu" class="nav-bar__menu" name="nav-bar__menu" onclick="window.menu.openMenu(event)">
+            <ul class="nav-bar__list">
+                <li><a class="nav-bar__link" href="#">HOME</a></li>
+                <li><a class="nav-bar__link" href="#">ABOUT US</a></li>
+                <li><a class="nav-bar__link" href="#">SERVICE</a></li>
+                <li><a class="nav-bar__link" href="#">CONTACT US</a></li>
+            </ul>
+        </div>
+        <div class="nav-bar__buttons">
+            ${HeaderButtons()}
+        </div>
     </nav>
     `
+}
+window.menu=[]
+window.menu.openMenu = (event)=>{
+    const $menuCheckbox = event.target
+    const $menuItens = document.querySelector('.nav-bar__list')
+    if($menuCheckbox.checked){
+        $menuItens.style.display="flex"
+    } else{
+        $menuItens.style.display="none"
+    }
+
 }
 
 export default NavBar
